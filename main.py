bbfd1fde9d59673fec4890670565eb6ceafa0af6
Seb <83839857+sebseb0802@users.noreply.github.com>
2023-07-08 20:12:02 +0100

    Fully functional
    - Fully functioning now
    - Only thing left would be to add a few more file extensions to be sorted

--- main.py
+++ main.py
@@ -11,13 +11,10 @@ file_extensions = {
     "mov": "Videos",
     "txt": "Text Files"
 }
-
-"""for filename in os.listdir(directory):
-    f = os.path.join(directory,filename)
-    if os.path.isfile(f):
-        print(f)"""
         
 for filename in os.listdir(directory):
     for k, v in file_extensions.items():
         if filename.endswith(k):
-            shutil.move(directory, f"\\{v}\\{filename}")
+            destination_folder = os.path.join(directory, v)
+            os.makedirs(destination_folder, exist_ok=True)  # Create the destination folder if it doesn't exist
+            shutil.move(os.path.join(directory, filename), os.path.join(directory, v, filename))
